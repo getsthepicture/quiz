20a8e7c1f0cb22f43bd8e3c1fde29de072cbbbe1
Laurence Wingo <qaneuro@Laurences-MacBook-Pro-3.local>
2018-02-15 17:53:38 -0500

    added code for the interface builder objects as well as some logic within the IBAction functions

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,20 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //IBOutlet variables that got connected through interface builder
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    //end of IBOutlet variables that got connected through interface builder
+    
+    //example variable
+    var doodleLable: UILabel!
+    //end of example variable
+    
+    //model that should be broken out
+    let questions: [String] = ["From what is cognac made?", "What is 7+7?", "What is the capital of Georgia?"]
+    let answers: [String] = ["Grapes", "14", "Atlanta"]
+    var currentQuestionIndex: Int = 0
+    //end of model that should be broken out
+    
+    //IBAction functions that got connected through interface builder
+    @IBAction func showAnswer(sender: AnyObject) {
+        
     }
     
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        
+        //when the showNextQuestionButton is tapped, increment currentQuestionIndex
+        currentQuestionIndex+=1
+        
+        //if statement saying if the currentQuestionIndex is equal to the number of elements in the questions array, then reset the currentQuestionIndex integer to zero so that the list of questions can start over
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        //create a variable called question which is a string, and it is equal to a specific string element within the questions array.  This specific element from the array is set by the currentQuestionIndex integer.
+        let question: String = questions[currentQuestionIndex]
+        
+        //set the text of questionLabel which is an instance of UILabel, equal to the string variable called question
+        questionLabel.text = question
+        
+        //reset the text of answerLabel which is a UILabel, equal to a string of question marks to indicate that the answer is hidden.
+        answerLabel.text = "???"
+        
+    }
+    //end of IBAction functions that got connected through interface builder
     
-    //myPlayArea
-
-
+    //example function
+    func runTheJewels(){
+        
+    }
+    //end of example function
+    
+    
+    
+    
 }
 
